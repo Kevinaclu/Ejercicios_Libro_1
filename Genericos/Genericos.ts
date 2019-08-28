4ee0306c68eae840e475786b179ea07cb4b7a43a
KevinAcLu <kevinaclu@gmail.com>
2019-08-27 20:01:45 -0600

    Genericos any

--- Genericos/Genericos.ts
+++ Genericos/Genericos.ts
@@ -1,6 +1,10 @@
-function randomStrElem(theArray: string[]): string {
+function randomElem(theArray: any[]): any {
     let randomIndex = Math.floor(Math.random() * theArray.length);
     return theArray[randomIndex];
 }
+let positions = [103, 458, 472, 458];
+let randomPosition = randomElem(positions);
+
+// This code will compile without an error.
 let colors: string[] = ['violet', 'indigo', 'blue', 'green'];
-let randomColor: string = randomStrElem(colors);
+let randomColor: number = randomElem(colors);
